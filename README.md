ffd99eb7c9afa847155b29006da23ba2d2eb156d
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-12-08 05:51:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+EncodeAndDecodeXML
+===========
+
+Two useful methods for stings to encode and decode html/xml elements
+
+How to use
+----------
+
+	string.encodeXML();
+	string.decodeXML();
+
+That simple!
---
+++ Source/EncodeAndDecodeXML.js
@@ -0,0 +1,44 @@
+/*
+---
+description: Two useful methods for stings to encode and decode html/xml elements
+
+authors:
+  - Jean-Nicolas Boulay Desjardins (http://jean-nicolas.name)
+
+license:
+  - MIT-style license
+
+requires:
+ - core/1.3:   '*'
+
+provides:
+  - encodeXML
+  - decodeXML
+...
+*/
+
+String.implement({
+    encodeXML: function(){  
+        var xml_to_escaped = {
+            '&': '&amp;',
+            '"': '&quot;',
+            '<': '&lt;',
+            '>': '&gt;'
+        };
+        return String(this).replace(/([\&"<>])/g, function(str, item) {
+            return xml_to_escaped[item];
+        });
+    },
+    decodeXML: function(){
+        var escaped_to_xml = {
+            '&amp;': '&',
+            '&quot;': '"',
+            '&lt;': '<',
+            '&gt;': '>'
+        };
+        
+        return String(this).replace(/(&quot;|&lt;|&gt;|&amp;)/g, function(str, item) {
+            return escaped_to_xml[item];
+        });
+    }
+});
